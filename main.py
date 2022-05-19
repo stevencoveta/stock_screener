63b2f4b6b184a0603d07338044ebb7c824cb7f65
Steve <39433211+stevencoveta@users.noreply.github.com>
2022-05-19 10:16:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,38 +9,25 @@ import streamlit as st
 
 
 #@st.cache(ttl=600)
-def option_info(ticker):
+def option_info(ticker,date):
     try:
-        msft = yf.Ticker(ticker)
-        opt = msft.option_chain(msft.options[0])[0]
-        stock_price = get_live_price(ticker)
-        closestStrike = opt.strike[(np.abs(opt.strike - stock_price)).argmin()]
-        option_chain = opt[opt.strike == closestStrike].reset_index(drop = True)
-        df = pd.concat([ pd.DataFrame({"StockPrice": get_live_price(ticker)},index=[0]),option_chain], axis = 1)
-        df["spread%"] = ((df["ask"] - df["bid"]) / df["bid"]).values[0]
-        df["mid"] = (round(((df["ask"] - df["bid"]) /2),2) + df["bid"]).values[0]
-        df = df[["StockPrice","contractSymbol","strike","lastPrice","bid","ask","spread%","mid","change","percentChange","volume","openInterest","impliedVolatility"]]
-        lst.append(df)
-        return df
-    except:
-        st.write(f"No ticker found for {ticker}")
+        msft = yf.Ticker(ticker,date)
+        if date == False:
+            strike_date = msft.options[0]
+            opt = msft.option_chain(strike_date)[0]
+        else: 
+            strike_date = date
+            opt = msft.option_chain(strike_date)[0]
 
-def option_info_strike(ticker,date):
-    try:
-        msft = yf.Ticker(ticker)
-        opt = msft.option_chain(date)[0]
-        stock_price = get_live_price(ticker)
-        closestStrike = opt.strike[(np.abs(opt.strike - stock_price)).argmin()]
-        option_chain = opt[opt.strike == closestStrike].reset_index(drop = True)
-        df = pd.concat([ pd.DataFrame({"StockPrice": get_live_price(ticker)},index=[0]),option_chain], axis = 1)
-        df["spread"] = ((df["ask"] - df["bid"]) / df["bid"]).values[0]
-        df["mid"] = ((df["ask"] - df["bid"]) + df["bid"]).values[0]
-        df = df[["StockPrice","contractSymbol","strike","lastPrice","bid","ask","spread","mid","change","percentChange","volume","openInterest","impliedVolatility"]]
-        lst.append(df)
+        option_chain = pd.DataFrame(opt[opt.inTheMoney == False].iloc[0]).T.reset_index(drop = True)
+        df = pd.concat([ pd.DataFrame({"Ticker":ticker,"StrikeDate":strike_date,"StockPrice": get_live_price(ticker)},index=[0]),option_chain], axis = 1)
+        df["spread%"] = ((df["ask"] - df["bid"]) / df["bid"]).values[0]
+        df["mid"] = ((df["ask"] - df["bid"]) /2 + df["bid"]).values
+        df = df[["Ticker","StrikeDate","StockPrice","contractSymbol","strike","lastPrice","bid","ask","spread%","mid","change","percentChange","volume","openInterest","impliedVolatility"]]
         return df
     except:
         st.write(f"No ticker found or strike date incorrect for {ticker}")
-             
+    
 
 def convert_df(dfs):
     return dfs.to_csv().encode('utf-8')
@@ -55,8 +42,12 @@ genre = st.sidebar.radio(
      ('Custom Data', 'Screener'))
 
 
+pwd = "0001"
 
-if genre == 'Custom Data':
+pwd = st.sidebar.text_input('Input Password')
+
+
+if genre == 'Custom Data' and pwd == "0001":
     agree = st.checkbox('Use custom strike dates')
     bt = st.button('Run')
     uploaded_file = st.file_uploader("Choose a file")
@@ -69,14 +60,15 @@ if genre == 'Custom Data':
             lst = []
             tickers = tickers_excel.Symbol.dropna().values
             tickers = tickers[tickers != "Cash"]
-            tickers = tickers[:101]
-            dates = tickers_excel["Date Added"].dropna().values
+            tickers = tickers
+            dates = tickers_excel["Strike Date"].dropna().values
             dt = [pd.to_datetime(dates[i]).strftime("%Y-%m-%d") for i in range(len(dates))]
-            dt = dt[:101]
+            dt = dt
             st.write(f"Downloading {len(tickers)} stocks from custom file with dates provided ")
+            #my_bar = st.progress(0)
             for i in range(len(tickers)):
-                st.progress(i)
-                lst.append(option_info_strike(tickers[i],dt[i]))
+                st.progress(i/len(tickers))
+                lst.append(option_info(tickers[i],dt[i]))
             main_df = pd.concat(lst).drop_duplicates()
             st.write(main_df)
             csv = convert_df(main_df)
@@ -90,11 +82,11 @@ if genre == 'Custom Data':
             lst = []
             tickers = tickers_excel.Symbol.dropna().values
             tickers = tickers[tickers != "Cash"]
-            tickers = tickers[:101]
+            tickers = tickers
             st.write(f"Downloading {len(tickers)} stocks from custom file ")
             for i in range(len(tickers)):
-                st.progress(i)
-                lst.append(option_info(tickers[i]))
+                st.progress(i/len(tickers))
+                lst.append(option_info(tickers[i],False))
                 
             main_df = pd.concat(lst).drop_duplicates()
             st.write(main_df)
@@ -105,7 +97,7 @@ if genre == 'Custom Data':
                     file_name='optionPrices.csv',
                     mime="text/csv")
 
-elif genre == 'Screener':
+elif genre == 'Screener' and pwd == "0001":
     option = st.selectbox(
     'What data would you like to use',
     ('None','Dow', 'SP500'))
@@ -116,7 +108,7 @@ elif genre == 'Screener':
         df1 = pd.DataFrame( si.tickers_dow() )
         st.write(f"Downloading {len(df1)} stocks from {option} ")
         for i in range(len(df1)):
-            st.progress(i)
+            st.progress(i/len(df1))
             lst.append(option_info(df1.values[i][0]))
         main_df = pd.concat(lst).drop_duplicates()
         st.write(main_df)
@@ -124,14 +116,14 @@ elif genre == 'Screener':
         st.download_button(
                     label="Download file data ",
                     data=csv,
-                    file_name='optionPrices.csv',
+                    file_name=f'optionPrices{option}.csv',
                     mime="text/csv")
     
     elif option == "SP500":
         df2 = pd.DataFrame( si.tickers_sp500() )
         st.write(f"Downloading {len(df2)} stocks from {option} ")
         for i in range(len(df2)):
-            st.progress(i)
+            st.progress(i/len(df2))
             lst.append(option_info(df2.values[i][0]))
         main_df = pd.concat(lst).drop_duplicates()
         st.write(main_df)
@@ -139,7 +131,7 @@ elif genre == 'Screener':
         st.download_button(
                     label="Download file data ",
                     data=csv,
-                    file_name='optionPrices.csv',
+                    file_name=f'optionPrices{option}.csv',
                     mime="text/csv")
         
     
